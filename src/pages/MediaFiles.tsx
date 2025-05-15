d61feb28504f83c5039fea122f097978fd2e7308
VibeSys <vibesys.sistemas@gmail.com>
2025-05-15 20:27:16 -0300

    Update MediaFiles.tsx

--- src/pages/MediaFiles.tsx
+++ src/pages/MediaFiles.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { supabase } from "../lib/supabaseClient";
+import { supabase } from "../integrations/supabase/client";
 
 type Midia = {
   id: string;
